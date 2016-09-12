5bfcbe8ece4b9efe4275b5826b0b608e4fd39161
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-09-11 22:51:52 -0500

    cleanup

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const override = require('method-override');
 const config = require('../config/config');
-//clientUrl
+
 module.exports = function(app) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
